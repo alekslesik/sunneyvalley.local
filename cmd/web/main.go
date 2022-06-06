014dd1798b92d16b817be604f6cff8fcd106a30e
alekslesik <alekslesik@gmail.com>
2022-06-06 13:08:21 +0300

    add and set up sql connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -17,8 +20,11 @@ func main() {
 
 	// create a new flag cmd
 	addr := flag.String("addr", ":4000", "Net address HTTP")
-	// call func for extract flag from cmd
 
+	// new cmd flag for setting MySQL connection
+	dsn := flag.String("dsn", "web:ndJMv9zrJw@/snippetbox?parseTime=true", "Name of MySQL data source")
+
+	// call func for extract flag from cmd
 	flag.Parse()
 	// log file
 	f, err := os.OpenFile("info.log", os.O_RDWR|os.O_CREATE, 0666)
@@ -28,10 +34,17 @@ func main() {
 	defer f.Close()
 
 	// set application
-	app := application{
+	app := &application{
 		errorLog: log.New(f, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
 		infoLog:  log.New(f, "INFO\t", log.Ldate|log.Ltime),
 	}
+
+	// open DB
+	db, err := openDB(*dsn)
+	if err != nil {
+		app.errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	// initialise new struct, set fields Addr and Handler
 	srv := &http.Server{
@@ -72,4 +85,18 @@ func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
 	}
 
 	return f, nil
+}
+
+// wrapping sql.Open() and return connection pull sql.DB
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
