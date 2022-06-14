be7618d7ed01a9b4ebe95ae896f9c0517908444e
alekslesik <alekslesik@gmail.com>
2022-06-14 14:49:10 +0300

    // storage for all dynamic data whitch should pass to HTML patterns

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,7 +3,7 @@ package main
 import "golangs.org/snippetbox/pkg/models"
 
 
-// storage for all dynamic data
+// storage for all dynamic data whitch should pass to HTML patterns
 type templateData struct {
 	Snippet *models.Snippet
 }
