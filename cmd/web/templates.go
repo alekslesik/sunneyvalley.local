815e1799a69b0bca9a7478d4afa9547db47e931c
alekslesik <alekslesik@gmail.com>
2022-06-24 16:14:56 +0300

    refactor

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,9 +1,13 @@
 package main
 
-import "golangs.org/snippetbox/pkg/models"
+import (
+	"golangs.org/snippetbox/pkg/models"
+	"golangs.org/snippetbox/pkg/models/page"
+)
 
 // storage for all dynamic data whitch should pass to HTML patterns
 type templateData struct {
+	PageData *page.PageData
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
 }
