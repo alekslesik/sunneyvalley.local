22fba5b514f48d7390dc81b63cc66bd887f7cff2
alekslesik <alekslesik@gmail.com>
2022-07-26 22:39:09 +0300

    async realise by sync.WaitGroup

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"sync"
 
 	"github.com/alekslesik/snippetbox/pkg/models"
 	"github.com/alekslesik/snippetbox/pkg/models/page"
@@ -20,11 +21,9 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	templateDir := "/root/go/src/github.com/alekslesik/snippetbox/template"
 	templateIncludesDir := "/root/go/src/github.com/alekslesik/snippetbox/template/includes"
 	componentsDir := "/root/go/src/github.com/alekslesik/snippetbox/pkg/components"
-
 	// init new map keeping cache
 	cache := map[string]*template.Template{}
-
-	// use func Glob to get all filepathes slice with '.page.html' ext
+	var wg sync.WaitGroup
 
 	// get dir before index.html
 	pages, err := filepath.Glob(filepath.Join(dir, "/*/index.html"))
@@ -33,42 +32,51 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	}
 
 	for _, page := range pages {
-		// get dir before index.html
-		name := filepath.Dir(page)
-		name = filepath.Base(name)
+		wg.Add(1)
 
-		// handle page
-		ts, err := template.ParseFiles(page)
-		if err != nil {
-			return nil, err
-		}
+		go func(page string) (interface{}, error) {
+			defer wg.Done()
+			// get dir before index.html
+			name := filepath.Dir(page)
+			name = filepath.Base(name)
 
-		// use ParseGlob to add all frame patterns (base.layout.html)
-		ts, err = ts.ParseGlob(filepath.Join(templateDir, "*.layout.html"))
-		if err != nil {
-			return nil, err
-		}
+			// handle page
+			ts, err := template.ParseFiles(page)
+			if err != nil {
+				return nil, err
+			}
 
-		// footer, header
-		ts, err = ts.ParseGlob(filepath.Join(templateDir, "*.partial.html"))
-		if err != nil {
-			return nil, err
-		}
+			// use ParseGlob to add all frame patterns (base.layout.html)
+			ts, err = ts.ParseGlob(filepath.Join(templateDir, "*.layout.html"))
+			if err != nil {
+				return nil, err
+			}
 
-		// all from /includes
-		ts, err = ts.ParseGlob(filepath.Join(templateIncludesDir, "*.html"))
-		if err != nil {
-			return nil, err
-		}
+			// footer, header
+			ts, err = ts.ParseGlob(filepath.Join(templateDir, "*.partial.html"))
+			if err != nil {
+				return nil, err
+			}
 
-		ts, err = ts.ParseGlob(filepath.Join(componentsDir, "*/*.html"))
-		if err != nil {
-			return nil, err
-		}
+			// all from /includes
+			ts, err = ts.ParseGlob(filepath.Join(templateIncludesDir, "*.html"))
+			if err != nil {
+				return nil, err
+			}
 
-		// add received patterns set to cache, using page name
-		cache[name] = ts
+			ts, err = ts.ParseGlob(filepath.Join(componentsDir, "*/*.html"))
+			if err != nil {
+				return nil, err
+			}
+
+			// add received patterns set to cache, using page name
+			cache[name] = ts
+
+			return cache, nil
+		}(page)
 	}
+
+	wg.Wait()
 
 	return cache, nil
 }
